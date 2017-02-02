289d159573a5d5e1ae54649ccc78afcf2a900e43
Jakub Mlynarczyk <jkbmlynarczyk@gmail.com>
2017-02-02 14:10:18 +0100

    Implement 2/3 of sudoku valid check

--- sudoku.py
+++ sudoku.py
@@ -65,3 +65,29 @@ class Sudoku(object):
         # For non-zero values, set a Cell as inmutable.
         if grid[ri][ci]:
           self._sudoku[ri].append(self._Cell(grid[ri][ci], False))
+
+  def is_valid(self):
+    # Three rules of valid Sudoku:
+    # 1. No row should have duplicated values.
+    # 2. No column should have duplicated values.
+    # 3. No 3x3 grid should have dupliacted values.
+
+    # Validate the requirement no. 1.
+    for r in self._sudoku:
+      if len(set(r)) != len(r):
+        return False
+
+    # Validate the requirement no. 2.
+    for ri in range(_SUDOKU_SIZE):
+      # Obtain and add values from each row.
+      c_list = []
+      c_set = set()
+      for ci in range(_SUDOKU_SIZE):
+        c_list.append(self._sudoku[ri][ci])
+        c_set.add(self._sudoku[ri][ci])
+
+      if len(set(c_set)) != len(c_list):
+        return False
+
+    # Validate the requirement no. 3.
+    # TODO(jakubm): Workout the mapping for squares.
