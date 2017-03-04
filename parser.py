dd92516cdad8c365fccf6c27954be1dea16990c3
jedrzej <jbszczepaniak@gmail.com>
2017-03-04 19:17:11 +0100

    Parser from String implemented

--- parser.py
+++ parser.py
@@ -1,11 +1,21 @@
 import sudoku
 
+class IncorrectLengthException(Exception):
+    """Used when from_string parser gets string  with wrong size"""
+
 def from_string(sudoku_str):
+
   sudoku_str = ''.join(sudoku_str.split())
-  s = sudoku.Sudoku()
+  if len(sudoku_str) != 81:
+    raise IncorrectLengthException()
+
+  grid =  [[None for i in range(9)] for j in range(9)]
+
   for idx, val in enumerate(sudoku_str):
     row_idx = idx // 9
     col_idx = idx % 9
-    s.set(cell.Cell(val), sudoku.Position(row_idx, col_idx))
+    grid[row_idx][col_idx] = val
 
+  s = sudoku.Sudoku()
+  s.insert(grid)
   return s
--- parser_test.py
+++ parser_test.py
@@ -4,11 +4,20 @@ import sudoku
 
 class TestParser(unittest.TestCase):
 
-    def test_string_input(self):
+    def test_string_input_yields_sudoku(self):
       s_str = "000 000 000 000 000 000 000 000 000 \
                000 000 000 000 000 000 000 000 000 \
                000 000 000 000 000 000 000 000 000 "
       self.assertIsInstance(parser.from_string(s_str), sudoku.Sudoku)
+
+    def test_wrong_size_sudoku_raises_exception(self):
+      too_short = "0"
+      with self.assertRaises(parser.IncorrectLengthException):
+        parser.from_string(too_short)
+
+      too_long = "0" * 82
+      with self.assertRaises(parser.IncorrectLengthException):
+        parser.from_string(too_long)
 
 if __name__ == '__main__':
   unittest.main()
